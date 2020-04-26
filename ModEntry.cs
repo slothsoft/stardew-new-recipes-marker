9421b620899c4ea0da872ec7996b5ef1b6dce1ef
slothsoft <admin@slothsoft.de>
2020-04-26 09:44:37 +0200

    Add project files.

---
+++ ModEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+
+using StardewValley;
+using StardewValley.Menus;
+
+namespace NewRecipesMarker
+{ 
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Display.MenuChanged += this.doMagic;
+        }
+
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void doMagic(object sender, MenuChangedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+            if (e.NewMenu == null)
+                return;
+
+            if (Game1.activeClickableMenu is GameMenu menu)
+            {
+                foreach (IClickableMenu page in menu.pages)
+                {
+                    switch (page)
+                    {
+                        case CraftingPage craftingPage:
+                            this.Monitor.Log($"Found crafting page.", LogLevel.Debug);
+                            doStuffToPage(craftingPage);
+
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        private void doStuffToPage(CraftingPage craftingPage)
+        {
+//            craftingPage.draw = drawPlus;
+        }
+
+        public override void drawPlus(SpriteBatch b)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿
+Tutorial: 
+https://stardewvalleywiki.com/Modding:Modder_Guide/Get_Started
+https://stardewvalleywiki.com/Modding:Content_packs
+https://stardewvalleywiki.com/Modding:IDE_reference
+https://stardewvalleywiki.com/Modding:Modder_Guide/APIs
+https://github.com/AdamMcIntosh/StawdewValley
+https://stardewvalleywiki.com/Modding:Common_tasks
